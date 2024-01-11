aee149aa52932c61e41a1e55b4d97ab2b890d75d
Hashim Raza <108229556+HashimRaza11@users.noreply.github.com>
2024-01-11 18:37:33 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Hashim</div>
+          <div className="right">
+            <p>Havely Restaurant,Lucknow</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Hashim Raza</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Hashim Raza.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
